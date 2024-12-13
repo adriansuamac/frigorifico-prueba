84f42d4d55297fa269fc99f4a2bdb2d6f638931e
OnicarN <ebaudani@gmail.com>
2024-12-13 11:05:26 +0100

    Método explotar

--- Frigorifico.java
+++ Frigorifico.java
@@ -4,13 +4,15 @@ public class Frigorifico{
     private double temperatura;
     private boolean encendido;
 
-
+   //Primer constructor del proyecto 
     public Frigorifico(string elModelo, double laTemperatura, boolean esEncendido){
         this modelo = modelo;
         this temperatura = temperatura;
         this encendido = encendido;
     }
-
+    public void explotar (){
+        System.out.println("Boom");
+    }
 
 
 
