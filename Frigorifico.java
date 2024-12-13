64e80e47552e8c8531b6c349d0b7ca2f0df42f24
adrian <adrian@programa.com>
2024-12-13 11:00:04 +0100

    Commit inicial

---
+++ Frigorifico.java
@@ -0,0 +1,29 @@
+public class Frigorifico{
+
+    private String modelo;
+    private double temperatura;
+    private boolean encendido;
+
+
+    public Frigorifico(string elModelo, double laTemperatura, boolean esEncendido){
+        this modelo = modelo;
+        this temperatura = temperatura;
+        this encendido = encendido;
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+}
